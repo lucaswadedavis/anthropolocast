9fee12ba41b2895b859d54e14206f37dad089eb4
Luke Davis <lukedavis@google.com>
2018-11-12 09:55:39 -0800

    Add routes

---
+++ src/Callback.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Callback extends Component {
+  render() {
+    return (
+      <p>
+        loading...
+      </p>
+    );
+  }
+}
+
+export default Callback;
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import App from './App';
+//import Home from './Home';
+import Callback from './Callback';
+import Auth from './auth';
+import history from './history';
+
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+export default function makeMainRoutes () {
+  return (
+    <Router history={history} component={App}>
+      <div>
+        <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+        <Route path="/callback" render={(props) => {
+          handleAuthentication(props);
+          return <Callback {...props} />
+        }}/>
+      </div>
+    </Router>
+  );
+}
